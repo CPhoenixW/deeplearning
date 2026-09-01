7e06a4c4ad294f4f4c9da286e02238be693bede3
PhoenixW <130205461+CPhoenixW@users.noreply.github.com>
2026-09-01 23:16:29 +0800

    Export FedDMC attack implementations

--- article2/src/attacks/__init__.py
+++ article2/src/attacks/__init__.py
@@ -10,9 +10,15 @@ from .distance import (
     rewrite_minmax_uploads,
     rewrite_minsum_uploads,
 )
+from .feddmc_backdoor import (
+    FEDDMC_TARGET_LABEL,
+    apply_feddmc_trigger,
+    evaluate_feddmc_backdoor_asr,
+)
 from .gaussian import GaussianNoiseAttack
 from .label_flipping import LabelFlippingAttack
 from .lie import LieAttack, apply_lie_round, lie_parameters, rewrite_lie_uploads
+from .lit import LitAttack, apply_lit_round, lit_attack_metadata, rewrite_lit_uploads
 from .mixed import (
     MixedAttack,
     apply_mixed_round,
@@ -33,6 +39,7 @@ from .registry import (
     evaluate_attack,
     validate_attack_config,
 )
+from .scaling import ScalingAttack, scaling_attack_metadata
 from .sign_flipping import SignFlippingAttack
 
 __all__ = [
@@ -42,15 +49,20 @@ __all__ = [
     "ATTACK_REGISTRY",
     "ROUND_ATTACK_HOOKS",
     "BackdoorAttack",
+    "FEDDMC_TARGET_LABEL",
     "GaussianNoiseAttack",
     "LabelFlippingAttack",
     "LieAttack",
+    "LitAttack",
     "MaliciousClient",
     "MinMaxAttack",
     "MinSumAttack",
     "MixedAttack",
+    "ScalingAttack",
     "SignFlippingAttack",
+    "apply_feddmc_trigger",
     "apply_lie_round",
+    "apply_lit_round",
     "apply_minmax_round",
     "apply_minsum_round",
     "apply_mixed_round",
@@ -59,13 +71,17 @@ __all__ = [
     "create_attack_client",
     "evaluate_attack",
     "evaluate_backdoor_asr",
+    "evaluate_feddmc_backdoor_asr",
     "evaluate_mixed_attack",
+    "lit_attack_metadata",
     "mixed_attack_for_client",
     "mixed_attack_ids",
     "mixed_attack_metadata",
     "lie_parameters",
     "rewrite_lie_uploads",
+    "rewrite_lit_uploads",
     "rewrite_minmax_uploads",
     "rewrite_minsum_uploads",
+    "scaling_attack_metadata",
     "validate_attack_config",
 ]
